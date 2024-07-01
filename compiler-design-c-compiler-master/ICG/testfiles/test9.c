92f3163fb0b03a6f386193490648391c76875d37
CodewithRajDeep <deepraj6897@gmail.com>
2024-07-01 17:51:29 +0530

    Compiler GUI

---
+++ compiler-design-c-compiler-master/ICG/testfiles/test9.c
@@ -0,0 +1,16 @@
+// passing wrong parameters to a function
+
+#include <stdio.h>
+
+int callme(int x)
+{
+    return x;
+}
+
+/// @brief 
+void main()
+{
+    int a1 = 1, a2 = 2;
+
+    callme(a1, a2);
+}
